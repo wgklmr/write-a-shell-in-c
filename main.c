0293adb72608589b7253f42252aaa7f1ed47f236
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-06-09 09:53:17 +0900

    add getchar() to wait user command

--- main.c
+++ main.c
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 
+void lsh_loop(void) {
+  int status;
+  int c;
+
+  do {
+    printf("user > ");
+    c = getchar();
+    status = 1;
+  } while(status);
+}
+
 int main(int argc, char **argv) {
   lsh_loop();
 
